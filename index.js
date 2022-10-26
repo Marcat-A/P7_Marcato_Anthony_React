163160724a63318494365eae38ae8353616ff2f2
Marcat-A <anthony.marcato@gmail.com>
2022-10-26 14:56:19 +0200

    fetch and map datas

---
+++ index.js
@@ -0,0 +1,35 @@
+const menu = document.querySelector(".menu");
+
+menu.innerHTML = recipes
+  .map((recipe) => {
+    return `<div class="card">
+    <div class="image"></div>
+    <div class="description">
+      <div class="titleTime">
+        <h2 class="title">${recipe.name}</h2>
+        <span class="time"
+          ><i class="fa-regular fa-clock"></i>${recipe.time} min</span
+        >
+      </div>
+      <div class="recette">
+        <div class="ingredientsUsed">
+          ${recipe.ingredients
+            .map((ingredient) => {
+              return `<span class="ingredient">${ingredient.ingredient}<span>${
+                ingredient.quantity ? ": " + ingredient.quantity : ""
+              }${ingredient.unit ? ingredient.unit : ""}</span></span>`;
+            })
+            .join("")}
+        </div>
+        <div class="paragraph">
+        ${
+          recipe.description.length > 200
+            ? recipe.description.substring(0, 200) + "..."
+            : recipe.description
+        }
+        </div>
+      </div>
+    </div>
+  </div>`;
+  })
+  .join("");
