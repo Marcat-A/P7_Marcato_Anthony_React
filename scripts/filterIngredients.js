f7eafbe98bbca2a1a2728cb5952c1cea9fae96c9
Marcat-A <anthony.marcato@gmail.com>
2022-11-14 17:26:01 +0100

    stable

---
+++ scripts/filterIngredients.js
@@ -0,0 +1,43 @@
+const dropdown = document.querySelector(".dropdownIngredients");
+// Dropdown
+const icon = document.querySelector(".icon1");
+// Icon
+
+icon.addEventListener("click", displayDropdown);
+
+function displayDropdown() {
+  dropdown.classList.toggle("show");
+}
+// Dropdown function
+
+const filteredRecipes = recipes.map((recipe) => {
+  return recipe.ingredients;
+});
+
+const filteredIngredients = [];
+
+for (let i = 0; i < filteredRecipes[i].length; i++) {
+  filteredRecipes[i].filter((ingredient) => {
+    if (!filteredIngredients.includes(ingredient.ingredient.toLowerCase())) {
+      filteredIngredients.push(ingredient.ingredient.toLowerCase());
+    }
+  });
+}
+
+const div = document.querySelector(".dropdownIngredients");
+
+div.innerHTML = filteredIngredients
+  .map(
+    (ingredient) =>
+      `<input type="text" value="${ingredient}" onclick="addIngredient(this.value)" readonly  class="ingredientInput" />`
+  )
+  .join("");
+
+function addIngredient(e) {
+  document.querySelector(
+    ".searchedItems"
+  ).innerHTML += `<button type="text" readonly value=${e} class="itemSearched">
+  ${e}
+  <i class="fa-regular fa-circle-xmark" onclick="this.parentElement.remove()"></i>
+</button>`;
+}
