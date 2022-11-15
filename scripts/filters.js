4589f1622fb31f9d8f02388e2cdf0f10e2b2a952
Marcat-A <anthony.marcato@gmail.com>
2022-11-15 15:40:14 +0100

    Update filters.js

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,5 +1,7 @@
 let searchedArray = [];
 let taggedArray = [];
+let filteredTagArray = [];
+let multipleTagsArray = [];
 const searchBar = document.getElementById("search");
 
 const searchBarFilter = () => {
@@ -10,15 +12,34 @@ const searchBarFilter = () => {
   );
 };
 
-const tagsFilter = () => {};
-
+const tagsFilter = (tag) => {
+  if (taggedArray.length === 1) {
+    filteredTagArray = recipes.filter((recipe) => {
+      return recipe.ingredients.some((r) => {
+        return r.ingredient.toLowerCase().includes(tag);
+      });
+    });
+  } else {
+    for (let i = 0; i < taggedArray.length; i++) {
+      console.log(taggedArray[i].toLowerCase());
+      multipleTagsArray = filteredTagArray.filter((filter) => {
+        return filter.ingredients.some((f) => {
+          return f.ingredient
+            .toLowerCase()
+            .includes(taggedArray[i].toLowerCase());
+        });
+      });
+    }
+  }
+};
 searchBar.addEventListener("input", searchBarFilter);
 
 const addTag = () => {
   const tags = document.querySelectorAll(".itemSearched");
   tags.forEach((tag) => {
-    if (!taggedArray.includes(tag.innerText)) {
-      taggedArray.push(tag.innerText);
+    if (!taggedArray.includes(tag.innerText.toLowerCase())) {
+      taggedArray.push(tag.innerText.toLowerCase());
+      tagsFilter(tag.innerText.toLowerCase());
     }
   });
 };
